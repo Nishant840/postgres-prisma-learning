b928d37f94102eb6e8bf4bb7b2390b0d289e979f
Nishant840 <nishant7896545@gmail.com>
2025-12-24 23:24:30 +0530

    updating and getting data from db

--- prisma_Learning/src/index.ts
+++ prisma_Learning/src/index.ts
@@ -2,19 +2,48 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 
-async function insertUser(email:string,password:string,firstname:string,lastname:string){
-    const res = await prisma.user.create({
-        data:{
-            email,
-            password,
-            firstname,
-            lastname
-        },
-        select:{
-            email:true,
-            password:true
-        }
+// async function insertUser(email:string,password:string,firstname:string,lastname:string){
+//     const res = await prisma.user.create({
+//         data:{
+//             email,
+//             password,
+//             firstname,
+//             lastname
+//         },
+//         select:{
+//             email:true,
+//             password:true
+//         }
+//     })
+//     console.log(res);
+// }
+// insertUser("nk2@gmail.com","123","nishant","kumar")
+
+
+// updating data in db
+// interface UpdateParams{
+//     firstname: string,
+//     lastname: string
+// }
+// async function updateUser(username:string,{firstname,lastname}:UpdateParams){
+//     const res = await prisma.user.update({
+//         where:{email:username},
+//         data:{
+//             firstname,
+//             lastname
+//         }
+//     })
+//     console.log(res);
+// }
+// updateUser("nk1@gmail.com",{
+//     firstname: "chhotu",
+//     lastname: "kumar"})
+
+// getting user details
+async function getUserDetails(username:string){
+    const res = await prisma.user.findMany({
+        where:{email:username}
     })
     console.log(res);
 }
-insertUser("nk2@gmail.com","123","nishant","kumar")
+getUserDetails("nk1@gmail.com");
